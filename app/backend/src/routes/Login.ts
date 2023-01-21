3229db3176c3fc8b729ad8f89e333805858e18fc
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-01-21 01:25:03 -0300

    feat: login/validate route implemented

--- app/backend/src/routes/Login.ts
+++ app/backend/src/routes/Login.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import auth from '../middleware/Auth';
 import loginMiddleware from '../middleware/Login';
 import LoginController from '../controllers/Login';
 
 const router = Router();
 const controller = new LoginController();
 
+router.get('/validate', auth, controller.validate.bind(controller));
 router.post('/', loginMiddleware, controller.post.bind(controller));
 
 export default router;
