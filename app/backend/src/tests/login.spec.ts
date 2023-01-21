ffde5c4533633d46035f271bc7184a715ac35973
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-01-20 23:59:04 -0300

    test: created integration test for login

---
+++ app/backend/src/tests/login.spec.ts
@@ -0,0 +1,57 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/User';
+import { user } from './mocks/Login';
+import * as jwt from 'jsonwebtoken';
+import { IJWTLogin } from '../interfaces/Login';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Test Login integration', () => {
+  afterEach(sinon.restore);
+
+  it('Should be able to login', async () => {
+    sinon.stub(User, 'findOne').resolves(user as any);
+    
+    const response = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'ofamoso16ton@famoso.com', password: '16toneladas' });
+
+    const validToken = jwt
+      .verify(
+        response.body.token,
+        process.env.JWT_SECRET as jwt.Secret
+      ) as IJWTLogin;
+      
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.have.property('token');
+    expect(validToken.email).to.be.equal('ofamoso16ton@famoso.com');
+  });
+  it('Should return an error - user entered wrong email', async () => {
+    const response = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'ofamoso26ton@famoso.com', password: '16toneladas' });
+      
+      
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.be.deep.equal({ message: 'Incorrect email or password' });
+  });
+  it('Should return an error - user entered wrong password', async () => {
+    const response = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'ofamoso16ton@famoso.com', password: '26toneladas' });
+      
+      
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.be.deep.equal({ message: 'Incorrect email or password' });
+  });
+});
