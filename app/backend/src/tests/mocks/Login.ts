6f6c2802579b0053d76d1d3ff964aef6f79816a2
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-01-21 00:02:13 -0300

    test: created mock file for login test

---
+++ app/backend/src/tests/mocks/Login.ts
@@ -0,0 +1,9 @@
+export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAdXNlci5jb20iLCJwYXNzd29yZCI6InNlY3JldF91c2VyIiwiaWF0IjoxNjc0MjYwNjg5fQ.JyUWflQk7zxXVZOMggox5-ydhFMFOkGj7T4shUcBFzs'
+
+export const user = {
+  id: 1,
+  user: '16 Toneladas',
+  email: 'ofamoso16ton@famoso.com',
+  role: 'user',
+  password: '$2a$10$c8P.lQlGJFJ1xA9LHDZEQ.eAhIyswtfKzLtC4y14rnB952X94qw2e'
+}
