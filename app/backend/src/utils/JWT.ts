4e2ca9b11d5c75653c67b24a299799bcafa02c00
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-01-21 01:21:59 -0300

    feat: add token verification

--- app/backend/src/utils/JWT.ts
+++ app/backend/src/utils/JWT.ts
@@ -4,11 +4,16 @@ import { Secret, SignOptions } from 'jsonwebtoken';
 import { IUser } from '../interfaces/Login';
 
 export default class JWT {
-  private _secret: Secret = process.env.JWT_SECRET as Secret;
+  private _secret: Secret = process.env.JWT_SECRET || 'jwt_secret';
   private _config: SignOptions = { algorithm: 'HS256', expiresIn: '1h' };
 
   public createToken(payload: IUser) {
     const token = jwt.sign({ ...payload }, this._secret, this._config);
     return token;
+  }
+
+  public verify(token: string) {
+    const decoded = jwt.verify(token, this._secret);
+    return decoded;
   }
 }
